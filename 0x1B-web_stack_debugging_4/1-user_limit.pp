8f1a5e29efbdc15472c0ecd7ac0516f32ab3a24d
elisadimiti <elisadimiti5@gmail.com>
2022-06-03 14:29:43 +0300

    Updated

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# Change the OS configuration so you login with the holberton user 
+exec { 'hard limit':
+  command => "sed -i 's/5/4000/' /etc/security/limits.conf",
+  path    => '/bin'
+}
+exec { 'soft limit':
+  command => "sed -i 's/4/2000/' /etc/security/limits.conf",
+  path    => '/bin'
+}
